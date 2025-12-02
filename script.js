abc48ce3293034a02722b9838393a8689b1f2ce5
kingedoh <kingedoh2012@gmail.com>
2025-12-02 18:01:06 +0100

    Add new image URL to the gallery

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const images = [
     //https://raw.githubusercontent.com/kingedoh/Edoh-King-/main/IMG-20250731-WA0008.jpg
     //https://raw.githubusercontent.com/kingedoh/Edoh-King-/main/IMG-20250731-WA0010.jpg
     //https://raw.githubusercontent.com/kingedoh/Edoh-King-/main/IMG_0832.JPG
+    //https://raw.githubusercontent.com/kingedoh/Edoh-King-/main/IMG_1040.JPG
 ];
 
 const grid = document.getElementById("gallery-grid");
